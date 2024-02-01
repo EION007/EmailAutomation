225bfd414584a1c8cc133165e64ba9fc04071b7e
eion <rolvinjm@gmail.com>
2024-02-01 19:06:04 +0530

    added api code and server code

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // app.js
 const { excelFilePath, ExcelReader } = require("./components/ExcelConfig");
 const { displayPartners } = require("./components/ExcelDisplay");
+const { fetchData } = require("./API/tenantData");
 
 async function runApp() {
   try {
-    const data = await ExcelReader.readExcel(excelFilePath);
-    displayPartners(data);
+    const tenantData = await ExcelReader.readExcel(excelFilePath);
+    displayPartners(tenantData);
+    // console.log("Fetched Data:", tenantData);
+    console.log("Fetched Data:", tenantData[0]);
+
+    const data = await fetchData("http://localhost:3001/data");
   } catch (error) {
     console.error("Error reading Excel file:", error.message);
   }
--- components/ExcelDisplay.js
+++ components/ExcelDisplay.js
@@ -1,8 +1,10 @@
 // DisplayLogic.js
-async function displayPartners(data) {
-  data.forEach(({ id, partner, tpt }) => {
-    console.log(`ID: ${id}, Partner: ${partner}, TPT: ${tpt}`);
-  });
+async function displayPartners(data, index = 0) {
+  const { id, partner, tpt } = data[index];
+  console.log(`ID: ${id}, Partner: ${partner}, TPT: ${tpt}`);
+  // data.forEach(({ id, partner, tpt }) => {
+  //   console.log(`ID: ${id}, Partner: ${partner}, TPT: ${tpt}`);
+  // });
 }
 
 module.exports = {
